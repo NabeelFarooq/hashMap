a0e87b15fba00f1a44b48dc2745c46936463502e
Nabeel Farooq <nabeel007dar@gmail.com>
2024-12-28 12:51:16 +0530

    Add files via upload

--- app.js
+++ app.js
@@ -32,9 +32,9 @@ class HashMap {
     const primeNumber = 31;
     for (let i = 0; i < string.length; i++) {
       hashCode = primeNumber * hashCode + string.charCodeAt(i);
+      hashCode = hashCode % this.arr.length;
     }
 
-    hashCode = hashCode % this.arr.length;
     return hashCode;
   }
 
